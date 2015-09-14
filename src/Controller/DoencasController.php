00686e4c849e39aa5e9d79bf7a7965311bcf64bc
Marcos Roberto <marcosdectba@gmail.com>
2015-09-14 20:53:09 -0300

    beta da leitura do XML do CID10

--- src/Controller/DoencasController.php
+++ src/Controller/DoencasController.php
@@ -34,6 +34,30 @@ class DoencasController extends AppController{
 	
 	public function importar(){
 		
+		//carregando XML do CID10
+		$xml = simplexml_load_file('C:\xampp2\htdocs\CID10XML\CID10.xml');
+		echo 'Capitulo 1, grupo 1, categoria 1...<br>';
+		echo $xml->capitulo[0]->grupo[0]->categoria[0]->nome.'<br>';
+		echo $xml->capitulo[0]->grupo[0]->categoria[0]['codcat'].'<br>';//Imprimindo dentro de capitulo>grupo>categoria[indice da categoria][codigo da categoria]
+		
+		echo $xml->capitulo[0]->grupo[0]->categoria[1]->nome.'<br>';
+		echo $xml->capitulo[0]->grupo[0]->categoria[1]['codcat'].'<br>';
+		
+		echo $xml->capitulo[0]->grupo[0]->categoria[2]->nome.'<br>';
+		echo $xml->capitulo[0]->grupo[0]->categoria[2]['codcat'].'<br>';
+		echo '-----------------------------------------------------------<br>';
+		echo 'Capitulo 2, grupo 1, categoria 2<br>';
+		echo $xml->capitulo[1]->grupo[0]->categoria[0]->nome.'<br>';
+		echo $xml->capitulo[1]->grupo[2]->categoria[0]['codcat'].'<br>';
+		/*
+		$aux = 0;
+		while($aux =! null){
+			$aux = $xml->capitulo['numcap'];
+			echo $aux;
+		}*/
+		
+		
+		
     }
 
 }
